b90e22dd07f4f719ba00dcfb70540a3863eea4a6
Raguraman . <ragur@sutherlandglobal.com>
2026-09-17 22:12:01 +0530

    ids

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     "name": "prod",
     "properties": {
         "production": true,
-        "ssdURL": "https://motor-api.neutrinos-apps.com/api/",
+        "ssdURL": "https://motor-ui.neutrinos-apps.com/api/",
         "tenantName": "neutrinos-training",
         "appName": "motor-poc-ui",
         "namespace": "com.neutrinos-training.motor-poc-ui",
